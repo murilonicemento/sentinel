2564be94f1336bb2917f222af20b03d98463a271
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-06 19:30:13 -0300

    feat: create infrastructure service collection extension

---
+++ services/ingestion/src/Ingestion.Infrastructure/InfrastructureServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Ingestion.Domain.Repositories;
+using Ingestion.Infrastructure.DbContext;
+using Ingestion.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ingestion.Infrastructure;
+
+public static class InfrastructureServiceCollectionExtension
+{
+    public static IServiceCollection AddInfrastructureServiceCollection(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IngestionDbContext>()
+            .AddScoped<IDataSourceRepository, DataSourceRepository>();
+    }
+}
