ac9c83885016b2c5aab0eb6b46e6ac67a70b83f2
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-06 19:27:26 -0300

    feat(middleware): create exception handling middleware

---
+++ services/ingestion/src/Ingestion.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace Ingestion.Api.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Unhandled exception occurred.");
+            await HandleException(httpContext, exception);
+        }
+    }
+
+    private static async Task HandleException(HttpContext httpContext, Exception exception)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        await httpContext.Response.WriteAsJsonAsync(new
+        {
+            error = exception.Message,
+            type = exception.GetType().Name
+        });
+    }
+}
