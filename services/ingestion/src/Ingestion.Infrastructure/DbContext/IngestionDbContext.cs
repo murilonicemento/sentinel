6e09f5f0250189d74f9982cf6204f06e360b5368
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-06 19:29:42 -0300

    feat(db): create db context

---
+++ services/ingestion/src/Ingestion.Infrastructure/DbContext/IngestionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Ingestion.Infrastructure.DbContext;
+
+public class IngestionDbContext
+{
+    private readonly IConfiguration _configuration;
+    public NpgsqlConnection Connection { get; }
+
+    public IngestionDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        Connection = CreateConnection();
+    }
+
+    private NpgsqlConnection CreateConnection() =>
+        new(_configuration.GetConnectionString("IngestionDatabase") ?? string.Empty);
+}
