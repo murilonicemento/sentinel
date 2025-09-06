38193572b85565a199bde13bc264cc59a0d400eb
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-06 19:28:59 -0300

    feat(repository): create datasource repository

---
+++ services/ingestion/src/Ingestion.Infrastructure/Repositories/DataSourceRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Ingestion.Domain.AggregateRoots;
+using Ingestion.Domain.Repositories;
+using Ingestion.Infrastructure.DbContext;
+
+namespace Ingestion.Infrastructure.Repositories;
+
+public class DataSourceRepository : IDataSourceRepository
+{
+    private readonly IngestionDbContext _context;
+
+    public DataSourceRepository(IngestionDbContext dbContext)
+    {
+        _context = dbContext;
+    }
+
+    public async Task<Guid> Register(DataSource dataSource)
+    {
+        const string query =
+            "INSERT INTO data_source (id, name, type, endpoint, collection_frequency) VALUES (@Id, @Name, @Type, @Endpoint, @CollectionFrequency)";
+
+        await _context.Connection.ExecuteAsync(query, dataSource);
+
+        return dataSource.Id;
+    }
+}
