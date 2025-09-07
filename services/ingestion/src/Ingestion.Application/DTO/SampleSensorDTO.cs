3e368bf299e9ffd61ddea914cc1b3ec94308b453
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-07 17:57:04 -0300

    feat(validations): add data annotations validations

--- services/ingestion/src/Ingestion.Application/DTO/SampleSensorDTO.cs
+++ services/ingestion/src/Ingestion.Application/DTO/SampleSensorDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace Ingestion.Application.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+using Ingestion.Application.Attributes;
 
-public record SampleSensorDTO(
-    double SensorValue,
-    string Unit,
-    DateTime RecordedAt
-);
+namespace Ingestion.Application.DTO;
+
+public record SampleSensorDTO
+{
+    [Required] public double SensorValue { get; set; }
+    [Required] public string Unit { get; set; } = string.Empty;
+    [Required] [FutureDate] public DateTime RecordedAt { get; set; }
+}
