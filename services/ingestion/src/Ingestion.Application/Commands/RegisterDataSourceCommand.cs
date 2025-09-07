db53db1cb00b2e4c7f467931e4f1d2763169519b
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-07 17:55:25 -0300

    refactor(command): change command properties

--- services/ingestion/src/Ingestion.Application/Commands/RegisterDataSourceCommand.cs
+++ services/ingestion/src/Ingestion.Application/Commands/RegisterDataSourceCommand.cs
@@ -1,11 +1,14 @@
-﻿using Ingestion.Domain.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using Ingestion.Domain.ValueObjects;
 using MediatR;
 
 namespace Ingestion.Application.Commands;
 
-public record RegisterDataSourceCommand(
-    string Name,
-    DataSourceTypeEnum Type,
-    string Endpoint,
-    CollectionFrequencyEnum CollectionFrequency
-) : IRequest<Guid>;
+public record RegisterDataSourceCommand : IRequest<Guid>
+{
+    [Required] public string Name { get; set; } = string.Empty;
+    [Required] public string DataSourceType { get; set; } = string.Empty;
+    [Required] public string MeasurementType { get; set; } = string.Empty;
+    [Required] public string Endpoint { get; set; } = string.Empty;
+    [Required] public string CollectionFrequency { get; set; } = string.Empty;
+}
