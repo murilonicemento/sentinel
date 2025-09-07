9a8eee498cacc96cd32fe1adb48d994ba8e47913
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-07 17:56:12 -0300

    feat(command): create register sensor collection command

--- services/ingestion/src/Ingestion.Application/Commands/RegisterSensorCollectionCommand.cs
+++ services/ingestion/src/Ingestion.Application/Commands/RegisterSensorCollectionCommand.cs
@@ -1,11 +1,14 @@
-﻿using Ingestion.Application.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+using Ingestion.Application.Attributes;
+using Ingestion.Application.DTO;
 using MediatR;
 
 namespace Ingestion.Application.Commands;
 
-public record RegisterSensorCollectionCommand(
-    Guid DatasourceId,
-    DateTime CollectedAt,
-    string Payload,
-    ICollection<SampleSensorDTO> Samples
-) : IRequest<Guid>;
+public record RegisterSensorCollectionCommand : IRequest<Guid>
+{
+    [Required] public Guid DatasourceId { get; set; }
+    [Required] [FutureDate] public DateTime CollectedAt { get; set; }
+    [Required] public string Payload { get; set; } = string.Empty;
+    [Required] public IEnumerable<SampleSensorDTO> SampleSensors { get; set; } = [];
+}
