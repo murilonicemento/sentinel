091503638436b4bf5c481e850dc64c6ab979432a
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-07 17:57:57 -0300

    refactor(handler): add new properties to datasource entity

--- services/ingestion/src/Ingestion.Application/Handlers/RegisterDataSourceHandler.cs
+++ services/ingestion/src/Ingestion.Application/Handlers/RegisterDataSourceHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Ingestion.Application.Commands;
 using Ingestion.Domain.AggregateRoots;
 using Ingestion.Domain.Repositories;
+using Ingestion.Domain.ValueObjects;
 using MediatR;
 
 namespace Ingestion.Application.Handlers;
@@ -16,15 +17,15 @@ public class RegisterDataSourceHandler : IRequestHandler<RegisterDataSourceComma
 
     public async Task<Guid> Handle(RegisterDataSourceCommand request, CancellationToken cancellationToken)
     {
-        var dataSource = new DataSource
-        {
-            Id = Guid.NewGuid(),
-            Name = request.Name,
-            Type = Convert.ToInt32(request.Type),
-            Endpoint = request.Endpoint,
-            CollectionFrequency = Convert.ToInt32(request.CollectionFrequency)
-        };
+        var dataSource = new DataSource(
+            Guid.NewGuid(),
+            request.Name,
+            DataSourceType.From(request.DataSourceType).Value,
+            MeasurementType.From(request.MeasurementType).Value,
+            request.Endpoint,
+            CollectionFrequencyType.From(request.CollectionFrequency).Value
+        );
 
-        return await _dataSourceRepository.Register(dataSource);
+        return await _dataSourceRepository.RegisterAsync(dataSource);
     }
 }
