f86e5bb4a2913acc83df7941556728de5e4ad014
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-20 17:45:01 -0300

    chore(scripts): create init db scipts

---
+++ platform/init-db/1-database.sql
@@ -0,0 +1,10 @@
+﻿DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = 'ingestion'
+   ) THEN
+      CREATE DATABASE ingestion;
+END IF;
+END
+$$;
---
+++ platform/init-db/2-schema.sql
@@ -0,0 +1,42 @@
+﻿CREATE TABLE data_source
+(
+    id                   uuid PRIMARY KEY,
+    name                 varchar(100) NOT NULL,
+    endpoint             varchar(100),
+    data_source_type     varchar(20)  NOT NULL,
+    measurement_type     varchar(20),
+    collection_frequency varchar(20)  NOT NULL,
+    tenant_id            uuid NULL,
+    created_at           timestamptz  not null default now()
+);
+
+CREATE TABLE data_collection
+(
+    id             uuid PRIMARY KEY,
+    data_source_id uuid        NOT NULL REFERENCES data_source (id),
+    collected_at   timestamptz NOT NULL,
+    payload        jsonb       NOT NULL,
+    tenant_id      uuid NULL,
+    created_at     timestamptz not null default now(),
+    CONSTRAINT uq_datasource_collectedat UNIQUE (data_source_id, collected_at)
+);
+
+CREATE TABLE sensor_sample
+(
+    id                 uuid PRIMARY KEY,
+    data_collection_id uuid             NOT NULL REFERENCES data_collection (id),
+    sensor_value       double precision NOT NULL,
+    unit               varchar(20)      NOT NULL,
+    recorded_at        timestamptz      NOT NULL
+);
+
+CREATE TABLE outbox
+(
+    id           uuid PRIMARY KEY,
+    aggregate_id uuid NULL,
+    outbox_type  text    not null,
+    payload      jsonb   not null,
+    processed    boolean not null default false,
+    processed_at timestamptz NULL
+);
+CREATE INDEX idx_outbox_processed ON outbox (processed) WHERE processed = false;
