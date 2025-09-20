df6bef9a3b99df38d071414451f281ed89397099
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-20 19:57:31 -0300

    refactor: add tenant id to command

--- services/ingestion/src/Ingestion.Application/Commands/RegisterSensorCollectionCommand.cs
+++ services/ingestion/src/Ingestion.Application/Commands/RegisterSensorCollectionCommand.cs
@@ -8,6 +8,7 @@ namespace Ingestion.Application.Commands;
 public record RegisterSensorCollectionCommand : IRequest<Guid>
 {
     [Required] public Guid DatasourceId { get; set; }
+    [Required] public Guid TenantId { get; set; }
     [Required] [FutureDate] public DateTime CollectedAt { get; set; }
     [Required] public string Payload { get; set; } = string.Empty;
     [Required] public IEnumerable<SampleSensorDTO> SampleSensors { get; set; } = [];
