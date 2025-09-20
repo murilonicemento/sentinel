c72b5b06f0c59de29d77e7017e86cfb68a5aec46
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-20 19:58:22 -0300

    chore: add services to IoC container

--- services/ingestion/src/Ingestion.Infrastructure/InfrastructureServiceCollectionExtension.cs
+++ services/ingestion/src/Ingestion.Infrastructure/InfrastructureServiceCollectionExtension.cs
@@ -1,16 +1,43 @@
 ﻿using Ingestion.Domain.Repositories;
 using Ingestion.Infrastructure.DbContext;
 using Ingestion.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Minio;
 
 namespace Ingestion.Infrastructure;
 
 public static class InfrastructureServiceCollectionExtension
 {
-    public static IServiceCollection AddInfrastructureServiceCollection(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructureServiceCollection(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
     {
         return services
-            .AddScoped<IngestionDbContext>()
-            .AddScoped<IDataSourceRepository, DataSourceRepository>();
+            .AddSingleton<IngestionDbContext>()
+            .AddRepositories()
+            .AddMinioConfiguration(configuration);
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IDataSourceRepository, DataSourceRepository>()
+            .AddScoped<IDataCollectionRepository, DataCollectionRepository>()
+            .AddScoped<ISampleSensorRepository, SampleSensorRepository>();
+    }
+
+    private static IServiceCollection AddMinioConfiguration(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        return services.AddMinio(cfg =>
+        {
+            cfg.WithEndpoint(configuration["MinIO:Host"])
+                .WithCredentials(configuration["MinIO:AccessKey"], configuration["MinIO:SecretKey"])
+                .Build();
+        });
     }
 }
