18470944604e4896e5b4c2325f0a1226866a60ad
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-09-20 19:59:22 -0300

    feat: create object storage provider

---
+++ services/ingestion/src/Ingestion.Application/Providers/IObjectStorageProvider.cs
@@ -0,0 +1,8 @@
+﻿using Minio.DataModel.Response;
+
+namespace Ingestion.Application.Providers;
+
+public interface IObjectStorageProvider
+{
+    public Task<PutObjectResponse> UploadJsonAsync(string bucketName, string objectName, string payload);
+}
---
+++ services/ingestion/src/Ingestion.Infrastructure/Providers/MinioProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Ingestion.Application.Providers;
+using Minio;
+using Minio.DataModel.Args;
+using Minio.DataModel.Response;
+
+namespace Ingestion.Infrastructure.Providers;
+
+public class MinioProvider : IObjectStorageProvider
+{
+    private readonly IMinioClient _minioClient;
+
+    public MinioProvider(IMinioClient minioClient)
+    {
+        _minioClient = minioClient;
+    }
+
+    public async Task<PutObjectResponse> UploadJsonAsync(string bucketName, string objectName, string payload)
+    {
+        var bucketExistsArgs = new BucketExistsArgs().WithBucket(bucketName);
+        var bucketExist = await _minioClient.BucketExistsAsync(bucketExistsArgs);
+
+        if (!bucketExist)
+        {
+            var makeBucketArgs = new MakeBucketArgs().WithBucket(bucketName);
+            await _minioClient.MakeBucketAsync(makeBucketArgs);
+        }
+
+        var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(payload));
+        var putObjectArgs = new PutObjectArgs()
+            .WithBucket(bucketName)
+            .WithObject(objectName)
+            .WithStreamData(memoryStream)
+            .WithObjectSize(memoryStream.Length)
+            .WithContentType("application/json");
+
+        return await _minioClient.PutObjectAsync(putObjectArgs);
+    }
+}
