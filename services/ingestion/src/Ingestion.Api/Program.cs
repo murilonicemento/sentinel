c8b9cad9764edc75473df1ce19f95ecb85b2399e
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-10-04 19:22:51 -0300

    fix: add type map to dapper

--- services/ingestion/src/Ingestion.Api/Program.cs
+++ services/ingestion/src/Ingestion.Api/Program.cs
@@ -7,6 +7,8 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
 builder.Services.AddControllers();
 builder.Services
     .AddOpenApi()
