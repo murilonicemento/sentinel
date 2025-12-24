59d01ff9fa1655ba29a39dc24ed8aafc26e8ae22
Murilo Nascimento <murilo_nascimento22@outlook.com>
2025-12-24 18:18:02 -0300

    feat(query): create service collection to application layer

---
+++ services/risk-catalog/src/RiskCatalog.Application/ApplicationServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RiskCatalog.Application;
+
+public static class ApplicationServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationServiceCollection(this IServiceCollection services) =>
+        services
+            .AddMediatR(config =>
+            {
+                // config.RegisterServicesFromAssembly(typeof(RegisterDataSourceHandler).Assembly);
+            });
+}
