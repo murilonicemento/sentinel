b726e884e620029b95ba10bb6a1a5b87ad8d506d
Murilo Nascimento <murilo_nascimento22@outlook.com>
2026-01-18 18:41:36 -0300

    feat(dto): create dto to add regional parameters

---
+++ services/risk-catalog/src/RiskCatalog.Application/DTO/CreateRegionalRiskParameterDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RiskCatalog.Application.DTO;
+
+public record CreateRegionalRiskParameterDTO
+{
+    [Required] public double AdjustmentFactor { get; set; }
+    [Required] public string Description { get; set; }
+}
